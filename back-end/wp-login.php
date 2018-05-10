d8c29802d138485393086eac3128c4ef98d931f6
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-05-10 19:39:30 +0200

    Removed some plugins, added new fields

--- back-end/wp-login.php
+++ back-end/wp-login.php
@@ -181,7 +181,7 @@ function login_header( $title = 'Log In', $message = '', $wp_error = '' ) {
 	do_action( 'login_header' );
 	?>
 	<div id="login">
-		<h1><a href="<?php echo esc_url( $login_header_url ); ?>" title="<?php echo esc_attr( $login_header_title ); ?>" tabindex="-1"><?php echo $login_header_text; ?></a></h1>
+		<h1><div class="custom-logo"></div></h1>
 	<?php
 
 	unset( $login_header_url, $login_header_title );
